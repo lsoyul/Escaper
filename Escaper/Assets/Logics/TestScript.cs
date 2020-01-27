e6d24a7ef2e8728209c4300a5d082562a695c5e6
sgykim15 <sgykim36@gmail.com>
2020-01-27 23:32:14 +0900

    - 맵 테스팅 스크립트 추가 - 배경 색 어둡게 수정

---
+++ Escaper/Assets/Logics/TestScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject[] levels;
+    public int currentLevel = 0;
+
+    public void ChangeTileLevel()
+    {
+        if (++currentLevel >= levels.Length) currentLevel = 0;
+        ActiveLevel(currentLevel);
+    }
+
+    void ActiveLevel(int level)
+    {
+        if (levels.Length > level)
+        {
+            for (int i=0; i<levels.Length; i++)
+            {
+                if (i == level) levels[i].SetActive(true);
+                else levels[i].SetActive(false);
+            }
+        }
+    }
+}
